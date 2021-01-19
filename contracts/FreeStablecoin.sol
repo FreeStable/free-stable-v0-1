08cf37676f3c143546d947a59ecb295d1f4d56b6
gildor <73232606+remote-gildor@users.noreply.github.com>
2021-01-19 01:27:08 +0100

    burn stablecoins & tests

--- contracts/FreeStablecoin.sol
+++ contracts/FreeStablecoin.sol
@@ -40,7 +40,7 @@ contract FreeStablecoin is ERC20, Ownable {
 
   // PUBLIC (state changing)
   function mintStablecoin() payable public returns(bool) {
-    _mintStablecoin(msg.value, msg.sender);
+    _mintStablecoin(msg.value, _msgSender());
     return true;
   }
 
@@ -49,9 +49,15 @@ contract FreeStablecoin is ERC20, Ownable {
     return true;
   }
 
-  // function burnStablecoin() public returns(bool) {}
+  function burnStablecoin(uint stablecoinAmount) public returns(bool) {
+    _burnStablecoin(stablecoinAmount, _msgSender());
+    return true;
+  }
   
-  // function burnStablecoinFor(address beneficiary) public returns(bool) {}
+  function burnStablecoinFor(uint stablecoinAmount, address beneficiary) public returns(bool) {
+    _burnStablecoin(stablecoinAmount, beneficiary);
+    return true;
+  }
 
   function getEthPrice() public returns(uint) {
     // gets current ETH price from an oracle
@@ -69,9 +75,39 @@ contract FreeStablecoin is ERC20, Ownable {
     vaults[_beneficiary] = Vault(_ethAmount, stablecoinAmount);
   }
 
-  // function _burnStablecoin(uint ethAmount, address beneficiary) internal {}
-    // burn fee should be taken in ETH (not in the stablecoin!)
-    // prevent burn if collateralization ratio below threshold
+  function _burnStablecoin(uint _stablecoinAmount, address _beneficiary) internal {
+    require(_stablecoinAmount > 0);
+    require(_beneficiary != address(0));
+
+    // check if msg.sender has enough stablecoins
+    uint senderBalance = balanceOf(_msgSender());
+
+    if (senderBalance == 0) {
+      revert("Sender's token balance is 0."); // if msg.sender has 0 stablecoins, revert
+    } else if (senderBalance < _stablecoinAmount) {
+      _stablecoinAmount = senderBalance; // balance is less than specified amount, reduce the _stablecoinAmount
+    }
+
+    // calculate the percentage of burned stablecoins in debt: (_stablecoinAmount / debt) * collateral
+    uint ratio = (_stablecoinAmount.mul(100)).div(getDebtAmount(_beneficiary));
+    uint ethUnlocked = (ratio.mul(getCollateralAmount(_beneficiary))).div(100);
+
+    // calculate the burn fee and reduce the amount of ETH to be returned
+    uint burnFee = ethUnlocked.mul(burnFeePercentage).div(100);
+
+    // stablecoin burn
+    _burn(_beneficiary, _stablecoinAmount);
+
+    // reduce the collateral and stablecoin amounts in Vault
+    vaults[_beneficiary].ethLocked = vaults[_beneficiary].ethLocked.sub(ethUnlocked);
+    vaults[_beneficiary].stablecoinsMinted = vaults[_beneficiary].stablecoinsMinted.sub(_stablecoinAmount);
+
+    // send the burn fee in ETH to the owner/governance address
+    payable(owner()).transfer(burnFee);
+
+    // return the unlocked ETH to beneficiary (minus the burn fee)
+    payable(_beneficiary).transfer(ethUnlocked.sub(burnFee));
+  }
 
   // GOVERNANCE
 
@@ -81,11 +117,11 @@ contract FreeStablecoin is ERC20, Ownable {
 
   // RECEIVE & FALLBACK
   receive() external payable { 
-    _mintStablecoin(msg.value, msg.sender);
+    _mintStablecoin(msg.value, _msgSender());
   }
 
   fallback() external payable {
-    _mintStablecoin(msg.value, msg.sender);
+    _mintStablecoin(msg.value, _msgSender());
   }
 
 }
--- test/FreeEUR.test.js
+++ test/FreeEUR.test.js
@@ -19,7 +19,8 @@ contract("FreeStablecoin", accounts => {
   const name = "FreeEUR";
   const symbol = "frEUR";
 
-  beforeEach(async () => {
+  before(async () => {
+    // before() is run only once at the start of the contract
     instance = await FreeStablecoin.new(name, symbol);
   });
 
@@ -48,17 +49,16 @@ contract("FreeStablecoin", accounts => {
   });
 
   describe("Minting and burning", () => {
+    let ethValue = 1;
+    let ethPrice = 500;
 
     it("mints stablecoins for the sender", async () => {
-      let ethValue = 1;
-      let ethPrice = 500;
-
       const mint = await instance.mintStablecoin({
         from: accounts[0],
         value: ether(ethValue)
       });
 
-      // gas used: 105810
+      // gas used: 105845
       // console.log("Gas used (mintStablecoin): " + mint.receipt.gasUsed);
 
       expectEvent(mint, "Transfer", {
@@ -72,12 +72,12 @@ contract("FreeStablecoin", accounts => {
 
       const debtAmount = await instance.getDebtAmount(accounts[0]);
       assert.equal(debtAmount, ether(ethValue*ethPrice));
+
+      const stablecoinBalance = await instance.balanceOf(accounts[0]);
+      assert.equal(stablecoinBalance, ether(ethValue*ethPrice));
     });
 
     it("mints stablecoins for another beneficiary", async () => {
-      let ethValue = 1;
-      let ethPrice = 500;
-
       // account 0 mints stablecoin for account 1
       const mint = await instance.mintStablecoinFor(accounts[1], {
         from: accounts[0],
@@ -98,10 +98,133 @@ contract("FreeStablecoin", accounts => {
 
       const debtAmount = await instance.getDebtAmount(accounts[1]);
       assert.equal(debtAmount, ether(ethValue*ethPrice));
+
+      const stablecoinBalance = await instance.balanceOf(accounts[1]);
+      assert.equal(stablecoinBalance, ether(ethValue*ethPrice));
     });
 
-    xit("", async () => {});
-    xit("", async () => {});
+    it("partly burns stablecoins", async () => {
+      const ethBalanceBefore = await web3.eth.getBalance(accounts[0]);
+
+      const stablecoinBalanceBefore = await instance.balanceOf(accounts[0]);
+      assert.equal(stablecoinBalanceBefore, ether(500));
+
+      let stablecoinsToBurn = ether(167);
+      
+      const burn = await instance.burnStablecoin(stablecoinsToBurn);
+
+      // gas used: 51354
+      // console.log("Gas used (burnStablecoin): " + burn.receipt.gasUsed);
+
+      expectEvent(burn, "Transfer", {
+        from: accounts[0],
+        to: constants.ZERO_ADDRESS,
+        value: stablecoinsToBurn
+      });
+
+      const collateralAmount = await instance.getCollateralAmount(accounts[0]);
+      assert.equal(collateralAmount, ether(0.67));
+
+      const debtAmount = await instance.getDebtAmount(accounts[0]);
+      assert.equal(debtAmount, ether(ethValue*ethPrice)-stablecoinsToBurn);
+
+      const stablecoinBalanceAfter = await instance.balanceOf(accounts[0]);
+      assert.equal(stablecoinBalanceAfter, ether(ethValue*ethPrice)-stablecoinsToBurn);
+
+      const ethBalanceAfter = await web3.eth.getBalance(accounts[0]);
+      assert.isTrue(ethBalanceBefore < ethBalanceAfter);
+    });
+
+    it("burns all the rest of the account 0's stablecoins", async () => {
+      const ethBalanceBefore = await web3.eth.getBalance(accounts[0]);
+
+      const stablecoinBalanceBefore = await instance.balanceOf(accounts[0]);
+      assert.equal(stablecoinBalanceBefore, ether(333));
+
+      let stablecoinsToBurn = ether(333);
+      
+      const burn = await instance.burnStablecoin(stablecoinsToBurn);
+
+      // gas used: 51354
+      // console.log("Gas used (burnStablecoin): " + burn.receipt.gasUsed);
+
+      expectEvent(burn, "Transfer", {
+        from: accounts[0],
+        to: constants.ZERO_ADDRESS,
+        value: stablecoinsToBurn
+      });
+
+      const collateralAmount = await instance.getCollateralAmount(accounts[0]);
+      assert.equal(collateralAmount, 0);
+
+      const debtAmount = await instance.getDebtAmount(accounts[0]);
+      assert.equal(debtAmount, 0);
+
+      const stablecoinBalanceAfter = await instance.balanceOf(accounts[0]);
+      assert.equal(stablecoinBalanceAfter, 0);
+
+      const ethBalanceAfter = await web3.eth.getBalance(accounts[0]);
+      assert.isTrue(ethBalanceBefore < ethBalanceAfter);
+    });
+
+    it("allows account 0 to burn stablecoins to benefit another user", async () => {
+      // account 0 needs to mint some frEUR because it doesn't have any right now
+      const mint = await instance.mintStablecoin({
+        from: accounts[0],
+        value: ether(0.5)
+      });
+
+      const ethBalanceSenderBefore = await web3.eth.getBalance(accounts[0]);
+
+      const ethBalanceBeneficiaryBefore = await web3.eth.getBalance(accounts[1]);
+
+      const stablecoinBalanceBefore = await instance.balanceOf(accounts[1]);
+      assert.equal(stablecoinBalanceBefore, ether(500));
+
+      let stablecoinsToBurn = ether(167);
+      
+      // account 0 burns their stablecoin to benefit account 1
+      const burn = await instance.burnStablecoinFor(stablecoinsToBurn, accounts[1]);
+
+      // gas used: 86257
+      // console.log("Gas used (burnStablecoinFor): " + burn.receipt.gasUsed);
+
+      expectEvent(burn, "Transfer", {
+        from: accounts[1],
+        to: constants.ZERO_ADDRESS,
+        value: stablecoinsToBurn
+      });
+
+      const collateralAmount = await instance.getCollateralAmount(accounts[1]);
+      assert.equal(collateralAmount, ether(0.67));
+
+      const debtAmount = await instance.getDebtAmount(accounts[1]);
+      assert.equal(debtAmount, ether(ethValue*ethPrice)-stablecoinsToBurn);
+
+      const stablecoinBalanceAfter = await instance.balanceOf(accounts[1]);
+      assert.equal(stablecoinBalanceAfter, ether(ethValue*ethPrice)-stablecoinsToBurn);
+
+      let burnFee = ether(0.0033);
+
+      // sender's ETH balance should go up by the burnFee amount (minus the tx fee) because sender is contract owner
+      const ethBalanceSenderAfter = await web3.eth.getBalance(accounts[0]);
+      assert.approximately(
+        Number(ethBalanceSenderAfter-ethBalanceSenderBefore), 
+        Number(burnFee),
+        Number(ether(0.0008)) // error of margin due to gas fee
+      );
+
+      // the beneficiary's ETH balance goes up for the unlocked collateral minus the burn fee
+      const ethBalanceBeneficiaryAfter = await web3.eth.getBalance(accounts[1]);
+      assert.approximately(
+        Number(ethBalanceBeneficiaryAfter-ethBalanceBeneficiaryBefore), 
+        Number(ether(0.33)-burnFee),
+        Number(82000) // error of margin due to dust
+      );
+    });
+
+    xit("burn but send an amount bigger than balance", async () => {});
+    xit("burn, but balance 0", async () => {});
     xit("", async () => {});
   });
 
